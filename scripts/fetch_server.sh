33926cad5c40c60e8a33856e32d0cedda0924916
Business Goose <44676012+business-goose@users.noreply.github.com>
2022-03-17 12:50:32 +0000

    Upgrade to Purpur 1.18.2 (closes #49)

--- scripts/fetch_server.sh
+++ scripts/fetch_server.sh
@@ -2,4 +2,4 @@
 # Script used to fetch the latest version of Paper
 
 mkdir -p fetched_server
-curl -L https://papermc.io/api/v1/paper/1.17.1/latest/download > fetched_server/server.jar
+curl -L https://api.purpurmc.org/v2/purpur/1.18.2/latest/download > fetched_server/server.jar
