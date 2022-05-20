3865f2edb5fea88e00d63e66e12b5331dbd438d2
kaboom <58372747+kaboombot@users.noreply.github.com>
2022-05-20 22:11:48 +0300

    Add ParticleTrails plugin

--- scripts/fetch_internal_plugins.sh
+++ scripts/fetch_internal_plugins.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Script used to fetch latest versions of internal plugins
-# Plugins: CommandSpy, Extras, iControlU, Weapons
+# Plugins: CommandSpy, Extras, iControlU, ParticleTrails, Weapons
 
 mkdir -p fetched_plugins
 
 for download_url in https://nightly.link/kaboomserver/commandspy/workflows/main/master/CommandSpy.zip \
                     https://nightly.link/kaboomserver/extras/workflows/main/master/Extras.zip \
                     https://nightly.link/kaboomserver/icontrolu/workflows/main/master/iControlU.zip \
+                    https://nightly.link/kaboomserver/particletrails/workflows/main/master/ParticleTrails.zip \
                     https://nightly.link/kaboomserver/weapons/workflows/main/master/Weapons.zip
 do
     curl -L $download_url > archive.zip
