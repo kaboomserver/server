b8d573fc97f8ad350cae50b6f846e3d850b811fb
kaboom <58372747+kaboombot@users.noreply.github.com>
2022-12-27 19:58:02 +0200

    Update Scissors and plugins

--- scripts/fetch_server.sh
+++ scripts/fetch_server.sh
@@ -3,7 +3,7 @@
 
 mkdir -p fetched_server
 
-curl -L https://ci.scissors.gg/job/Scissors/job/1.19.2/lastSuccessfulBuild/artifact/*zip*/archive.zip > archive.zip
+curl -L https://ci.scissors.gg/job/Scissors/job/1.19.3/lastSuccessfulBuild/artifact/*zip*/archive.zip > archive.zip
 unzip -o archive.zip
 mv archive/build/libs/Scissors-*.jar fetched_server/server.jar
 
