98c23badc82f30c98e541f108a2aaa0604bc1693
Kaboom <58372747+kaboombot@users.noreply.github.com>
2024-01-22 18:30:50 +0200

    Downgrade Scissors for now

--- scripts/fetch_server.sh
+++ scripts/fetch_server.sh
@@ -3,7 +3,7 @@
 
 mkdir -p fetched_server
 
-curl -L https://ci.plex.us.org/job/Scissors/job/1.20.4/lastSuccessfulBuild/artifact/*zip*/archive.zip > archive.zip
+curl -L https://ci.plex.us.org/job/Scissors/job/1.20.4/17/artifact/*zip*/archive.zip > archive.zip
 unzip -o archive.zip
 mv archive/build/libs/scissors-*.jar fetched_server/server.jar
 
