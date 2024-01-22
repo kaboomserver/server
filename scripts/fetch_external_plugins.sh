1bb60aa797d155f7dece677689ee12ecabbed092
Kaboom <58372747+kaboombot@users.noreply.github.com>
2024-01-22 17:56:46 +0200

    Use release builds of ViaVersion

--- scripts/fetch_external_plugins.sh
+++ scripts/fetch_external_plugins.sh
@@ -8,9 +8,9 @@ mkdir -p fetched_plugins
 for download_url in https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/*zip*/archive.zip \
                     https://ci.athion.net/job/FastAsyncWorldEdit/lastSuccessfulBuild/artifact/*zip*/archive.zip \
                     https://nightly.link/GeyserMC/Geyser/workflows/build/master/Geyser%20Spigot.zip \
-                    https://ci.viaversion.com/job/ViaVersion-DEV/lastSuccessfulBuild/artifact/*zip*/archive.zip \
-                    https://ci.viaversion.com/job/ViaBackwards-DEV/lastSuccessfulBuild/artifact/*zip*/archive.zip \
-                    https://ci.viaversion.com/job/ViaRewind-DEV/lastSuccessfulBuild/artifact/*zip*/archive.zip
+                    https://ci.viaversion.com/job/ViaVersion/lastSuccessfulBuild/artifact/*zip*/archive.zip \
+                    https://ci.viaversion.com/job/ViaBackwards/lastSuccessfulBuild/artifact/*zip*/archive.zip \
+                    https://ci.viaversion.com/job/ViaRewind/lastSuccessfulBuild/artifact/*zip*/archive.zip
 do
     curl -L $download_url > archive.zip
     unzip -o archive.zip
